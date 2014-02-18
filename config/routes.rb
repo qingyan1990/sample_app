2147caedb7aa95c7691e2d46711a61fa201b2464
qingyan1990 <liqingyan1990@sina.com>
2014-02-18 14:58:38 +0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin', to: 'sessions#new',   via: 'get'
+  match '/signout', to: 'sessions#destroy',   via: 'delete'
   match '/signup', to: 'users#new',   via: 'get'
   match '/help', to: 'static_pages#help',   via: 'get'
   match '/about', to: 'static_pages#about',   via: 'get'
